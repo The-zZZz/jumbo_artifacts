c49d9aa201a6fec0a980fb8dd3284764ee154754
The_zZz <jona.adt@gmail.com>
2025-08-15 14:11:51 +0200

    Added Curio API dependency

---
+++ src/main/java/net/thezzz/jumbo_artifacts/item/artifacts/wadingbootsItem.java
@@ -0,0 +1,10 @@
+package net.thezzz.jumbo_artifacts.item.artifacts;
+
+import net.minecraft.world.item.Item;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+public class wadingbootsItem extends Item implements ICurioItem {
+    public wadingbootsItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
